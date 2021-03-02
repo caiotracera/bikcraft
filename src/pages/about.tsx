daa3871751294a759076db33bdd2a02bf6abd90b
Caio Tracera <ctrcra@gmail.com>
2021-03-02 19:41:57 -0300

    feat: add about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,8 +2,20 @@ import React from 'react';
 import Link from 'next/link';
 
 import Header from '@/components/Header';
+import Break from '@/components/Break';
+import Footer from '@/components/Footer';
 
-import { Introduction } from '@/styles/pages/About';
+import {
+  Introduction,
+  Quality,
+  QualityItem,
+  AboutContainer,
+  ValuesList,
+  Team,
+} from '@/styles/pages/About';
+
+import BikcraftQualidade from '../assets/images/bikcraft-qualidade.svg';
+import equipeBikcraft from '../assets/images/equipe-bikcraft.jpg';
 
 const About: React.FC = () => {
   return (
@@ -15,6 +27,63 @@ const About: React.FC = () => {
           <p>conheça mais sobre a bikcraft</p>
         </div>
       </Introduction>
+
+      <AboutContainer className="container">
+        <div className="grid-10">
+          <h2 className="subtitulo-interno">História, Missão e Visão</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+            porro magnam culpa atque a soluta voluptate accusamus.Mollitia
+            veritatis debitis asperiores nesciunt sint, perspiciatis voluptate
+            doloremque optio vel blanditiis doloribus.
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Praesentium nam laudantium possimus, aperiam temporibus dolorem,
+            earum quia quidem officiis sunt ducimus magni in ea beatae? Quod
+            excepturi consectetur ex non.
+          </p>
+        </div>
+        <div className="grid-6">
+          <h2 className="subtitulo-interno">Valores</h2>
+          <ValuesList>
+            <li>- Qualidade no processo com</li>
+            <li>- Foco no cliente sem perder a</li>
+            <li>- Diversão, preservando a</li>
+            <li>- Natureza com sustentabilidade</li>
+          </ValuesList>
+        </div>
+
+        <Team className="grid-16">
+          <img src={equipeBikcraft} alt="Equipe Bikcraft" />
+        </Team>
+      </AboutContainer>
+
+      <Quality className="container">
+        <h2 className="subtitulo">Qualidade</h2>
+        <BikcraftQualidade />
+
+        <ul>
+          <QualityItem className="grid-1-3">
+            <h3>Durabilidade</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </QualityItem>
+
+          <QualityItem className="grid-1-3">
+            <h3>Design</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </QualityItem>
+
+          <QualityItem className="grid-1-3">
+            <h3>Sustentabilidade</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </QualityItem>
+        </ul>
+      </Quality>
+
+      <Break />
+
+      <Footer />
     </>
   );
 };
--- src/styles/pages/About/index.tsx
+++ src/styles/pages/About/index.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 import BackgroundImg from '../../../assets/images/bg-sobre.jpg';
+import linhasImg from '../../../assets/images/linhas.png';
 
 export const Introduction = styled.section`
   margin-top: 95px;
@@ -13,7 +14,7 @@ export const Introduction = styled.section`
   padding-top: 30px;
 
   h1 {
-    font-size: 48px;
+    font-size: 36px;
     color: #fff;
     font-weight: bold;
     text-transform: uppercase;
@@ -24,7 +25,7 @@ export const Introduction = styled.section`
       width: 60px;
       height: 3px;
       background: #fff;
-      margin: 14px auto 10px auto;
+      margin: 6px auto 10px auto;
     }
   }
 
@@ -33,4 +34,106 @@ export const Introduction = styled.section`
     line-height: 20px;
     color: #fff;
   }
+`;
+
+export const Quality = styled.section`
+  padding: 60px 0;
+
+  svg {
+    display: block;
+    margin: 0 auto;
+  }
+
+  div {
+    clear: both;
+    text-align: center;
+    padding-top: 40px;
+
+    p {
+      margin-bottom: 20px;
+    }
+  }
+
+  ul {
+    padding-top: 20px;
+    overflow: auto;
+  }
+
+  &:after {
+    content: '';
+    width: 634px;
+    height: 83px;
+    display: block;
+    background: url(${linhasImg}) no-repeat center;
+    position: absolute;
+    top: 209px;
+    right: 162px;
+    z-index: -5;
+  }
+`;
+
+export const QualityItem = styled.li`
+  text-align: center;
+  padding: 0 40px;
+
+  h3 {
+    font-size: 18px;
+    font-weight: bold;
+    line-height: 25px;
+    text-transform: uppercase;
+    letter-spacing: 0.1em;
+    margin-top: 20px;
+
+    &:after {
+      content: '';
+      display: block;
+      width: 60px;
+      height: 3px;
+      background: #000;
+      margin: 6px auto 10px auto;
+    }
+  }
+`;
+
+export const AboutContainer = styled.section`
+  padding-top: 60px;
+
+  h2.subtitulo-interno {
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 30px;
+    letter-spacing: 0.1em;
+    color: #000;
+    text-transform: uppercase;
+    margin-bottom: 20px;
+
+    &:after {
+      content: '';
+      display: block;
+      width: 60px;
+      height: 3px;
+      background: #000;
+      margin: 8px 0;
+    }
+  }
+
+  p {
+    font-size: 18px;
+    line-height: 25px;
+    margin-bottom: 1em;
+    padding-right: 60px;
+  }
+`;
+
+export const ValuesList = styled.ul`
+  li {
+    font-family: Georgia, 'Times New Roman', Times, serif;
+    font-size: 18px;
+    line-height: 30px;
+    padding-right: 60px;
+  }
+`;
+
+export const Team = styled.div`
+  padding-top: 20px;
 `;
